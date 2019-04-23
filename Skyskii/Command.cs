0e4c95697f1cdac3bf8e16aa465f2d179e6bf870
Sarah <MistaMishMash@users.noreply.github.com>
2019-04-23 15:47:20 +1000

    adding Command.cs file.

---
+++ Skyskii/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skyskii
+{
+    // purpose is to subdivide 'command' strings into
+    // action and target.
+    class Command
+    {
+    }
+}
