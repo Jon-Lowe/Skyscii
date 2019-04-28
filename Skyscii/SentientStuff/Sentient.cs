9d9756106d1748d365b0a87a22d9469fff014fa6
parasol-me <dnoble1994@gmail.com>
2019-04-28 18:30:32 +1000

    fix sentient references

--- Skyscii/SentientStuff/Sentient.cs
+++ Skyscii/SentientStuff/Sentient.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Skyskii.SentientStuff
+namespace Skyscii.SentientStuff
 {
     class Sentient : ITargetableObject, ISearchable
     {
