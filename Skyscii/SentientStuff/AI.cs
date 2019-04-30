5098b853b219248dc11316364c0ef0a41ab14ead
Sarah <MistaMishMash@users.noreply.github.com>
2019-04-30 18:32:01 +1000

    updated namespaces for classes within SentientStuff to all be Skyscii.SentientStuff

--- Skyscii/SentientStuff/AI.cs
+++ Skyscii/SentientStuff/AI.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Skyskii.Sentient
+namespace Skyscii.SentientStuff
 {
     class AI
     {
--- Skyscii/SentientStuff/Stats.cs
+++ Skyscii/SentientStuff/Stats.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Skyskii
+namespace Skyscii.SentientStuff
 {
     class Stats
     {
