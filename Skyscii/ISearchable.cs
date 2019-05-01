8899e7c3595b7d250d1c064b20bd2f88f0f12b4f
Sarah <MistaMishMash@users.noreply.github.com>
2019-05-01 11:05:09 +1000

    added description to interface.

--- Skyscii/ISearchable.cs
+++ Skyscii/ISearchable.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Skyscii
 {
+    /*
+     * an interface for any game object that contains other game objects that may be targeted by a command.
+     * eg, room, inventory
+     */
     public interface ISearchable {
 
         ITargetableObject findTarget(String name);
