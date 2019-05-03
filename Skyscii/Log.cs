099dcd7ac8266892f96988ff41128c60e45d0f2e
James H <jamesherrero@gmail.com>
2019-05-03 15:36:53 +1000

    Add Menu, Add MainMenu, Add Log

--- Skyscii/Log.cs
+++ Skyscii/Log.cs
@@ -8,5 +8,45 @@ namespace Skyscii
     // could be replaced with a standard log?
     public class Log
     {
+
+        private List<string> mainLog { get; set; }
+
+        public Log()
+        {
+            mainLog = new List<string>();
+        }
+
+        
+        public List<string> GetLog
+        {
+            get { return mainLog; }
+        }
+
+
+        public void Add(string action)
+        {
+            mainLog.Add(action);
+        }
+
+        public List<string> getLineOfLog(int number)
+        {
+            List<string> result = new List<string>();
+        
+
+            if (mainLog.Count > number)
+            {
+                for (int i = mainLog.Count - number; i < mainLog.Count; i++)
+                {
+                    result.Add(mainLog[i]);
+                }
+            }
+            else
+            {
+                result = mainLog;
+            }
+        
+            return result;
+        }
+
     }
 }
--- Skyscii/Menu.cs
+++ Skyscii/Menu.cs
@@ -5,7 +5,96 @@ using System.Text;
 namespace Skyscii
 {
     // could also be implemented as an interface
-    public abstract class Menu
+    abstract class Menu
     {
+        protected List<string> validActions = new List<string>();
+        protected List<string> globalActions = new List<string>();
+        protected bool quit = false;
+        protected string error = "";
+
+        // TEMPORARY - (for drawing log) Should end up using a number input or key based menu to page through lines
+        protected const int NUM_LINES = 10;
+
+        public bool Quit { get => quit; }
+
+        public Menu()
+        {
+            globalActions.Add("help");
+            globalActions.Add("log");
+        }
+
+        internal Menu ProcessGlobalCommand(string command, Log log)
+        {
+            //TEMPORARY - store every command in log for testing (replace with selective inside child menus)
+            if (validActions.Contains(command) || globalActions.Contains(command))
+            {
+                log.Add(command);
+            }
+
+            if (validActions.Contains(command))
+            {
+                return ProcessCommand(command, log);
+            }
+
+            Menu result = this;
+            switch (command)
+            {
+                case "help":
+                    DrawHelp();
+                    break;
+                case "log":
+                    DrawLog(log);
+                    break;
+                default:
+                    error = "Please type a valid command";
+                    break;
+            }
+            return result;
+        }
+
+        internal void DrawHelp()
+        {
+            Console.Clear();
+            Console.WriteLine("Menu actions");
+            Console.WriteLine("----------");
+            foreach (string s in validActions)
+            {
+                Console.WriteLine("(" + s + ")");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Global actions");
+            Console.WriteLine("----------");
+            foreach (string s in globalActions)
+            {
+                Console.WriteLine("(" + s + ")");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return");
+            Console.ReadKey();
+        }
+
+        internal void DrawLog(Log log)
+        {
+            Console.Clear();
+            List<string> lines = log.getLineOfLog(NUM_LINES);
+
+            Console.WriteLine("Last " + lines.Count + " lines of log");
+            Console.WriteLine("----------");
+            if (lines.Count > 0)
+            {
+                foreach (string s in lines)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to return");
+            Console.ReadKey();
+        }
+
+        internal abstract Menu ProcessCommand(string command, Log log);
+
+        internal abstract void Draw();
     }
 }
