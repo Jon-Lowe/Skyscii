2318cf7a72ec390c5ab5c8a55c39ce1c44dc6f99
parasol-me <dnoble1994@gmail.com>
2019-05-03 17:58:29 +1000

    clean up comments and make sure class diagram is added

--- Skyscii/Menu/Menu.cs
+++ Skyscii/Menu/Menu.cs
@@ -9,10 +9,6 @@ namespace Skyscii
     {
         protected List<string> validActions = new List<string>();
         protected List<string> globalActions = new List<string>();
-
-        // new room
-        // new player
-        // new enemy
         protected bool quit = false;
         protected string error = "";
 
